51d561abfad36d358806cf938df779253860f3be
tjaydkSchool <dennis.michael@jubii.dk>
2015-11-02 21:43:05 +0100

    Added some to JUnit test

---
+++ test/UserJUnitTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import entity.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dennis
+ */
+public class UserJUnitTest {
+    
+    public UserJUnitTest() {
+        User user = new User();
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
