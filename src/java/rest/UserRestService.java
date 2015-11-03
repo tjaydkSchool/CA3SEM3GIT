ab0ea0db19dbe1f3d37a9f3bd7a8e3546cceb243
andreassnorrason <Asnorrason@10.0.1.10>
2015-11-03 16:11:13 +0100

    Changed User classes

--- src/java/rest/UserRestService.java
+++ src/java/rest/UserRestService.java
@@ -1,54 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.User;
+import facade.UserFacade;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 
-/**
- * REST Web Service
- *
- * @author Dennis
- */
+
 @Path("user")
 public class UserRestService {
+    Gson gson;
+    UserFacade facade;
 
     @Context
     private UriInfo context;
 
-    /**
-     * Creates a new instance of UserRestService
-     */
+
     public UserRestService() {
+        gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
+        
+//        CHANGE PU ON DEPLOYMENT
+        facade = new UserFacade();
     }
 
     /**
-     * Retrieves representation of an instance of rest.UserRestService
+     * Get users company list on login
+     * 
      * @return an instance of java.lang.String
      */
     @GET
+    @Path("{username}")
     @Produces("application/json")
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public String getUserCompanyList(@PathParam("username") String username) {
+        return gson.toJson(facade.getUser(username));
     }
 
     /**
-     * PUT method for updating or creating an instance of UserRestService
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
+     * Create new user with username and password, then return response with success of failure and credentials
+     * @param content username and password
+     * @return an HTTP response with credentials.
      */
-    @PUT
+    @POST
     @Consumes("application/json")
-    public void putJson(String content) {
+    public void createUser(String userData) {
+        facade.createUser(gson.fromJson(userData, User.class));
     }
 }
--- test/UserJUnitTest.java
+++ test/UserJUnitTest.java
@@ -6,6 +6,7 @@
 
 import entity.Company;
 import entity.User;
+import facade.UserFacade;
 import java.awt.List;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,50 +20,79 @@ import static org.junit.Assert.*;
  * @author Dennis
  */
 public class UserJUnitTest {
-    
+
+    UserFacade facade;
+
     public UserJUnitTest() {
     }
-    
+
     @Before
     public void setUp() {
+        facade = new UserFacade();
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
+    /*
+     ===========================================================================
+     ================= START OF TEST OF ENTITY CLASS ===========================
+     ===========================================================================
+     */
     @Test
     public void testCreateUserEntity() {
         String username = "user";
         String password = "pass";
-        
+
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-        
+
         assertEquals(user.getUsername(), username);
         assertEquals(user.getPassword(), password);
-        
+
     }
-    
+
     @Test
     public void testAddCompanyToUserEntityCompanyList() {
         User user = new User();
         Company comp = new Company();
-        user.addCompanyToList(comp);
-        
+        user.addCompanyToList("9999");
+
         assertTrue(user.getCompanyList().size() > 0);
     }
-    
+
     @Test
     public void testRemoveCompanyFromUserEntityCompanyList() {
         User user = new User();
-        Company comp = new Company();
-        user.addCompanyToList(comp);
-        
+        user.addCompanyToList("9999");
+
         assertTrue(user.getCompanyList().size() > 0);
-        
-        user.removeCompanyFromList(comp);
+
+        user.removeCompanyFromList("9999");
         assertTrue(user.getCompanyList().size() == 0);
     }
+
+    /*
+     ================= END OF TEST OF ENTITY CLASS =============================
+     */
+    /*
+     ===========================================================================
+     ================= START OF TEST OF PERSISTENCE ============================
+     ===========================================================================
+     */
+    @Test
+    public void testCreateUserInDatabase() {
+        User user = new User("tester", "tester");
+        facade.createUser(user);
+        User userFromDB = facade.getUser("tester");
+        assertEquals(user.getUsername(), userFromDB.getUsername());
+        
+        facade.deleteUser(user.getUsername());
+    }
+
+    /*
+     ================= END OF TEST OF PERSISTENCE ==============================
+     */
 }
